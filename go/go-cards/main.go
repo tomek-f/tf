a8afad8ae3df178f438cd2e19f16ed72a65cc232
Tomek Fijoł <tomekfijol@gmail.com>
2023-04-13 13:46:00 +0200

    feat: go-cards newCard (#12)

--- go/go-cards/main.go
+++ go/go-cards/main.go
@@ -6,11 +6,15 @@ const elo string = "Elo!"
 
 func main() {
 	var card string = "Ace of Spades"
-	card2 := "Ace of Spades"
+	card2 := newCard()
 
 	card += " and some other card"
 
 	fmt.Println(card)
 	fmt.Println(card2)
 	fmt.Println(elo)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
