bdeadc81395ba881cbe1a0a9d39970948a50f5e3
Tomasz Fijoł <tomasz.fijol@bold.com>
2023-04-15 18:38:26 +0200

    feat: go cards - saveToFile

--- go/cards/main.go
+++ go/cards/main.go
@@ -1,11 +1,15 @@
 package main
 
 func main() {
+	// cards := newDeck()
+
+	// hand, remainingCards := deal(cards, 5)
+
+	// cards.print("All cards:")
+	// hand.print("\nHand:")
+	// remainingCards.print("\nRemaining cards:")
+
 	cards := newDeck()
-
-	hand, remainingCards := deal(cards, 5)
-
-	cards.print("All cards:")
-	hand.print("\nHand:")
-	remainingCards.print("\nRemaining cards:")
+	// fmt.Println(cards.toString())
+	cards.saveToFile("my_cards")
 }
