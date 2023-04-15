41830fc5d8c2f0e31c4f26ec1b870b381f5dbdc3
Tomasz Fijoł <tomasz.fijol@bold.com>
2023-04-15 15:05:20 +0200

    feat: go cards - deal

--- go/cards/main.go
+++ go/cards/main.go
@@ -1,7 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
 	cards := newDeck()
 
+	hand, remainingCards := deal(cards, 5)
+
+	fmt.Println("All cards:")
 	cards.print()
+	fmt.Println("\nHand:")
+	hand.print()
+	fmt.Println("\nRemaining cards:")
+	remainingCards.print()
 }
