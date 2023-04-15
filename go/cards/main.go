9aaac8f9a8c2c439e71cb89dfacce68292fde7bd
Tomasz Fijoł <tomasz.fijol@bold.com>
2023-04-15 15:14:44 +0200

    feat: go cards - print changes

--- go/cards/main.go
+++ go/cards/main.go
@@ -1,16 +1,11 @@
 package main
 
-import "fmt"
-
 func main() {
 	cards := newDeck()
 
 	hand, remainingCards := deal(cards, 5)
 
-	fmt.Println("All cards:")
-	cards.print()
-	fmt.Println("\nHand:")
-	hand.print()
-	fmt.Println("\nRemaining cards:")
-	remainingCards.print()
+	cards.print("All cards:")
+	hand.print("\nHand:")
+	remainingCards.print("\nRemaining cards:")
 }
