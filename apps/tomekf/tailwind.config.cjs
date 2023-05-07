a4097d1a3fc88bced56ce40a96789b9a78474ca6
Tomasz Fijoł <tomasz.fijol@bold.com>
2023-05-07 17:52:07 +0200

    feat: proper tailwind config

---
+++ apps/tomekf/tailwind.config.cjs
@@ -0,0 +1,25 @@
+// import type { Config } from 'tailwindcss';
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ['./src/**/*.{astro,tsx}', './src/styles/**/*.ts'],
+  theme: {
+    extend: {
+      screens: {
+        '3xl': '1920px',
+      },
+      keyframes: {
+        float: {
+          '0%': { transform: 'translate3d(0, 0, 0)' },
+          '100%': { transform: 'translate3d(0, 0.5rem, 0)' },
+        },
+      },
+      animation: {
+        float: 'float 1.5s linear infinite alternate',
+        'float-long': 'float 3s linear infinite alternate',
+      },
+    },
+  },
+  plugins: [],
+};
