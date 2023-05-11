789b9f1e4d782e8cfcad95bee887d0f92db31fc2
Tomasz Fijoł <tomasz.fijol@bold.com>
2023-05-11 19:41:28 +0200

    feat: use clsx

--- apps/react-playground/src/components/Button/Button.tsx
+++ apps/react-playground/src/components/Button/Button.tsx
@@ -1,4 +1,4 @@
-import classnames from 'classnames';
+import { clsx } from 'clsx';
 
 type Props = React.DetailedHTMLProps<
   React.ButtonHTMLAttributes<HTMLButtonElement>,
@@ -8,7 +8,7 @@ type Props = React.DetailedHTMLProps<
 const Button = (props: Props) => {
   return (
     <button
-      className={classnames(
+      className={clsx(
         'text-white bg-orange-500 hover:bg-yellow-500',
         'transition-colors duration-200 ease-in-out',
         'px-2 py-1 font-medium rounded',
