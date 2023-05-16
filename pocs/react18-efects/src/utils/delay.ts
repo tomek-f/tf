eb29b5112b5c525063f42378fa683cee8f06a349
Tomek Fijoł <tomekfijol@gmail.com>
2023-05-16 18:18:20 +0200

    feat: POCs changes, deps, lint next in actions (#63)

--- pocs/react18-efects/src/utils/delay.ts
+++
@@ -1,5 +0,0 @@
-const delay = <T = undefined>(ms: number, data?: T): Promise<T> =>
-  // eslint-disable-next-line no-promise-executor-return
-  new Promise((resolve) => setTimeout(() => resolve(data as T), ms));
-
-export default delay;
