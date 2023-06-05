2407dc0c9afe47961538d19f80d0cac9998cbb4c
Tomasz Fijoł <tomasz.fijol@bold.com>
2023-06-06 00:03:24 +0200

    feat: Add Zod poc form + style changes

--- pocs/zod/src/App.tsx
+++ pocs/zod/src/App.tsx
@@ -1,11 +1,14 @@
-import './App.css';
-
+import Form from './components/Form';
 import fetchUsers from './lib/fetchUsers';
 
 fetchUsers();
 
 function App() {
-  return <>hello</>;
+  return (
+    <main className="bg-slate-700 min-h-screen p-5">
+      <Form />
+    </main>
+  );
 }
 
 export default App;
