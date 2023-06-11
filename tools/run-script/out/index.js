962bf724477472d4463e992670a20fe6c87eceda
Tomasz Fijoł <tomasz.fijol@bold.com>
2023-06-11 15:04:21 +0200

    feat: [@tf/run-script] Remove version real 4

--- tools/run-script/out/index.js
+++ tools/run-script/out/index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import { execSync } from 'node:child_process';
 import { readFileSync } from 'node:fs';
+
 import gradient from 'gradient-string';
 import inquirer from 'inquirer';
 
@@ -8,18 +9,18 @@ var packageJson = JSON.parse(readFileSync('./package.json', 'utf-8'));
 var scripts = packageJson.scripts;
 var keys = Object.keys(scripts);
 var questions = [
-    {
-        type: 'list',
-        name: 'script',
-        message: 'Choose a script to run',
-        choices: keys,
-    },
+  {
+    type: 'list',
+    name: 'script',
+    message: 'Choose a script to run',
+    choices: keys,
+  },
 ];
 
 console.log(gradient('#bada55', 'hotpink')('Hi, welcome to run-script!'));
 inquirer.prompt(questions).then(function (answers) {
-    execSync('npm run '.concat(answers.script), {
-        // cwd: process.cwd(),
-        stdio: [process.stdin, process.stdout, process.stderr],
-    });
+  execSync('npm run '.concat(answers.script), {
+    // cwd: process.cwd(),
+    stdio: [process.stdin, process.stdout, process.stderr],
+  });
 });
