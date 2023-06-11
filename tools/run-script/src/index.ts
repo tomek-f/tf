66040a5b87a87534b1346f96942dc893053d8ae1
Tomek Fijoł <tomekfijol@gmail.com>
2023-06-11 13:18:33 +0200

    feat: [@tf/run-script] TS init version (#110)

--- tools/run-script/src/index.ts
+++ tools/run-script/src/index.ts
@@ -5,7 +5,12 @@ import { readFileSync } from 'node:fs';
 import gradient from 'gradient-string';
 import inquirer from 'inquirer';
 
-const packageJson = JSON.parse(readFileSync('./package.json', 'utf-8'));
+interface PackageJson {
+  scripts: Record<string, string>;
+  [x: string]: unknown;
+}
+
+const packageJson = JSON.parse(readFileSync('./package.json', 'utf-8')) as PackageJson;
 const scripts = packageJson.scripts;
 const keys = Object.keys(scripts);
 const questions = [
@@ -17,7 +22,9 @@ const questions = [
   },
 ];
 
-console.log(gradient('#bada55', 'hotpink')('Hi, welcome to run-script!'));
+console.log(
+  gradient('#bada55', 'hotpink')(`Hi, welcome to run-script@${process.env.npm_package_version}!`),
+);
 
 inquirer.prompt(questions).then((answers) => {
   execSync(`npm run ${answers.script}`, {
