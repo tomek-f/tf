7179346bdf174285f9cdbc6f7afda945968ba1ba
Tomasz Fijoł <tomasz.fijol@bold.com>
2023-06-12 08:02:21 +0200

    feat: [@tf/react-router-ssr] Change components

--- pocs/react-router-ssr/src/views/Root/Root.tsx
+++ pocs/react-router-ssr/src/views/Root/Root.tsx
@@ -1,11 +1,40 @@
-import { Main } from './Main/Main';
+import { Route, Routes } from 'react-router-dom';
 
 import styles from './Root.module.css';
 
+// const Root = () => <div>Root</div>;
+
 export const Root = () => {
   return (
-    <div className={styles.root}>
-      <Main />
-    </div>
+    <main className={styles.root}>
+      <Routes>
+        <Route element={<div>root</div>} path="/" />
+        <Route element={<div>a</div>} path="/a" />
+        <Route
+          element={
+            <div>
+              {/* nestest routes b/* */}
+              <Routes>
+                <Route
+                  element={
+                    <div>
+                      b/<em>default</em>
+                    </div>
+                  }
+                  path=""
+                />
+                <Route element={<div>b</div>} path="b" />
+                {/* conditional path */}
+                {!window && <Route element={<div>c</div>} path="c" />}
+                <Route element={<div>d</div>} path="d" />
+                <Route element={<div>not found</div>} path="*" />
+              </Routes>
+            </div>
+          }
+          path="/b/*"
+        />
+        <Route element={<div>not found</div>} path="*" />
+      </Routes>
+    </main>
   );
 };
