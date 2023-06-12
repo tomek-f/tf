0f494ed78f815235d50b2d5b6f686a56625cb39f
Tomasz Fijoł <tomasz.fijol@bold.com>
2023-06-12 08:27:57 +0200

    feat: [@tf/react-router-ssr] Add some changes

--- pocs/react-router-ssr/src/views/Root/Root.tsx
+++ pocs/react-router-ssr/src/views/Root/Root.tsx
@@ -2,38 +2,31 @@ import { Route, Routes } from 'react-router-dom';
 
 import styles from './Root.module.css';
 
-// const Root = () => <div>Root</div>;
+// const Elem = () => <div>elem</div>;
 
 export const Root = () => {
   return (
     <main className={styles.root}>
       <Routes>
-        <Route element={<div>root</div>} path="/" />
-        <Route element={<div>a</div>} path="/a" />
+        <Route element={<div>/</div>} path="/" />
+        <Route element={<div>/a</div>} path="/a" />
         <Route
           element={
             <div>
               {/* nestest routes b/* */}
               <Routes>
-                <Route
-                  element={
-                    <div>
-                      b/<em>default</em>
-                    </div>
-                  }
-                  path=""
-                />
-                <Route element={<div>b</div>} path="b" />
+                <Route element={<div>/b/default</div>} path="" />
+                <Route element={<div>/b/b</div>} path="b" />
                 {/* conditional path */}
-                {!window && <Route element={<div>c</div>} path="c" />}
-                <Route element={<div>d</div>} path="d" />
-                <Route element={<div>not found</div>} path="*" />
+                {!window && <Route element={<div>/b/c</div>} path="c" />}
+                <Route element={<div>/b/d</div>} path="d" />
+                <Route element={<div>/b/not-found</div>} path="*" />
               </Routes>
             </div>
           }
           path="/b/*"
         />
-        <Route element={<div>not found</div>} path="*" />
+        <Route element={<div>not-found</div>} path="*" />
       </Routes>
     </main>
   );
