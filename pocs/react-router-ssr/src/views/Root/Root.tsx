6be7b9c58a103cf91a8724819a0f7c0de478880b
Tomasz Fijoł <tomasz.fijol@bold.com>
2023-06-14 11:12:59 +0200

    feat: [@tf/react-router-ssr] Add chnages

--- pocs/react-router-ssr/src/views/Root/Root.tsx
+++ pocs/react-router-ssr/src/views/Root/Root.tsx
@@ -1,24 +1,36 @@
-import { Outlet, Route, Routes, useOutletContext, useParams } from 'react-router-dom';
+import {
+  Link,
+  matchPath,
+  Outlet,
+  Route,
+  Routes,
+  useLocation,
+  useMatch,
+  useOutletContext,
+  useParams,
+} from 'react-router-dom';
+
+import { history } from '../../router/history';
 
 import styles from './Root.module.css';
 
 const LoginLayout = () => (
   <div>
-    <p>LoginLayout</p>
+    <div>LoginLayout</div>
     <Outlet />
   </div>
 );
 
 const AdminLayout = () => (
   <div>
-    <p>AdminLayout</p>
+    <div>AdminLayout</div>
     <Outlet />
   </div>
 );
 
 const BooksLayout = () => (
   <div>
-    <p>BooksLayout</p>
+    <div>BooksLayout</div>
     <Outlet context={{ contextData: 'context data' }} />
   </div>
 );
@@ -29,7 +41,7 @@ const Bookid = () => {
 
   return (
     <div>
-      <p>Bookid</p>
+      <div>Bookid</div>
       <div>
         Book {id} {context.contextData}
       </div>
@@ -37,11 +49,40 @@ const Bookid = () => {
   );
 };
 
-// default with path="*" or index
+const Nested2Id = () => {
+  const params = useParams();
+
+  return (
+    <div>
+      <div>Nested2Id</div>
+      <Link to="../">../</Link>
+      <pre>{JSON.stringify(params, null, 2)}</pre>
+    </div>
+  );
+};
+
+// default with path="*", '/' or index
 
 export const Root = () => {
+  const location = useLocation();
+  const match1 = useMatch({ path: location.pathname });
+  const match2 = useMatch({ path: '/nested2/:id/', end: false });
+  const match3 = matchPath({ path: '/nested2/:id/:id2/elo/:id3', end: false }, location.pathname);
+  const match4 = matchPath({ path: '/nested2/:id', end: false }, location.pathname);
+  const match5 = matchPath({ path: '/nested3/:id', end: false }, location.pathname);
+  const match6 = matchPath({ path: '/e/:id', end: false }, location.pathname);
+  const match7 = matchPath({ path: '/e', end: false }, location.pathname);
+
+  console.log({ match1, match2, match3, match4, match5, match6, match7 });
+
+  // @ts-expect-error elo
+  window.elo = {};
+  // @ts-expect-error elo
+  window.elo.history = history;
+
   return (
     <main className={styles.root}>
+      <Link to="b/conditional">conditional</Link>
       <Routes>
         <Route
           element={
@@ -49,15 +90,23 @@ export const Root = () => {
               <Routes>
                 <Route element={<div>/</div>} path="/" />
                 <Route element={<div>/a</div>} path="/a" />
+                {!!window && (
+                  <>
+                    <Route element={<div>/c</div>} path="/c" />
+                    <Route element={<div>/d</div>} path="/d" />
+                  </>
+                )}
+                <Route element={<div>/b/a</div>} path="b/a" />
                 <Route
                   element={
-                    <div>
+                    <div className={styles.spec}>
+                      <Link to="/b/conditional">conditional</Link>
                       {/* nestest routes b/* */}
                       <Routes>
                         <Route element={<div>/b/default</div>} path="/" />
                         <Route element={<div>/b/inner</div>} path="inner" />
                         {/* conditional path */}
-                        {!window && (
+                        {!!window && (
                           <Route element={<div>/b/conditional</div>} path="conditional" />
                         )}
                         <Route element={<div>/b/not-found</div>} path="*" />
@@ -68,7 +117,8 @@ export const Root = () => {
                 />
                 <Route
                   element={
-                    <div>
+                    <div className={styles.spec}>
+                      <Link to="../">../</Link>
                       {/* Outlet */}
                       <Routes>
                         <Route element={<LoginLayout />}>
@@ -105,6 +155,17 @@ export const Root = () => {
                   <Route element={<div>/nested/old</div>} path="old" />
                   <Route element={<div>/nested/not-found</div>} path="*" />
                 </Route>
+                <Route path="/nested2/:id/*">
+                  <Route element={<Nested2Id />} path=":id2/elo/:id3" />
+                  <Route element={<div>/nested2/not-found</div>} path="*" />
+                </Route>
+                <Route path="/nested3/*">
+                  <Route element={<div>/nested3/new</div>} path="new" />
+                  <Route element={<div>/nested3/old</div>} path="old" />
+                  <Route element={<div>/nested3/not-found</div>} path="*" />
+                </Route>
+                <Route element={<div>e</div>} path="e" />
+                <Route element={<div>e/&lt;id&gt;</div>} path="e/:id" />
                 <Route element={<div>global not-found</div>} path="*" />
               </Routes>
               <div>component in router (rooter access) on every route</div>
