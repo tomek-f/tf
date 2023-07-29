9437f9151d0caae6eea374226cc17903b296a978
Tomek Fijoł <tomekfijol@gmail.com>
2023-07-29 10:40:17 +0200

    feat: Add favicons changes (#170)

--- pocs/next-playground/src/app/layout.tsx
+++ pocs/next-playground/src/app/layout.tsx
@@ -20,7 +20,7 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html className="dark" lang={config.language.split('-')[0]}>
       <link
-        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💀</text></svg>"
+        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👷🏼</text></svg>"
         rel="icon"
       />
       <body
--- pocs/next-playground/src/pages/_app.tsx
+++ pocs/next-playground/src/pages/_app.tsx
@@ -14,7 +14,7 @@ const App = ({ Component, pageProps }: AppProps) => {
       <Head>
         <title>Maelstrom</title>
         <link
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💀</text></svg>"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👷🏼</text></svg>"
           rel="icon"
         />
         <meta content="Default desc" name="description" />
