18e58bafd052a30ae0d444311312cacc251124cd
Tomasz Fijoł <tomasz.fijol@bold.com>
2023-08-07 14:45:00 +0200

    feat: [tomekf.pl] Astro changes

--- sites/tomekf/astro.config.ts
+++ sites/tomekf/astro.config.ts
@@ -1,26 +1,22 @@
+// TODO use https://docs.astro.build/en/guides/assets/ when it's not experimental
 import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-// import compress from 'astro-compress';
 // import critters from 'astro-critters';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tomekf.pl/',
+  compressHTML: true,
   build: { inlineStylesheets: 'always' },
   trailingSlash: 'never',
   integrations: [
     // critters(),
     tailwind({ applyBaseStyles: false }),
     image({ serviceEntryPoint: '@astrojs/image/sharp' }),
-    // compress({
-    //   // logger: 3,
-    //   html: { collapseWhitespace: true, removeComments: true },
-    //   svg: { multipass: true, floatPrecision: 1 },
-    // }),
     mdx(),
     sitemap({ filter: (page) => !page.includes('/blog') }),
     react(),
