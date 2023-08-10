be5374c503a6aab79eb1f722be98780b4b3e4411
Tomek Fijoł <tomekfijol@gmail.com>
2023-08-10 20:46:32 +0200

    feat: [tomekf] Add transitions to astro; Add link changes (#182)

--- sites/tomekf/src/styles/common.ts
+++ sites/tomekf/src/styles/common.ts
@@ -1,1 +1,7 @@
-export const linkCss = 'text-orange-500 hover:text-blue-500 text-base';
+export const linkDefaultColorCss = 'text-blue-500';
+
+export const linkActiveColorCss = 'text-orange-500';
+
+export const linkPartialCss = `hover:${linkDefaultColorCss} text-base`;
+
+export const linkOrangeCss = `${linkActiveColorCss} ${linkPartialCss}`;
