a849144afe1b65ecdf8699cfe03f0c2667d8236d
Tomasz Fijoł <tomasz.fijol@bold.com>
2023-09-05 10:50:49 +0200

    feat: [tomekf] Change config (sitemap)

--- sites/tomekf/astro.config.ts
+++ sites/tomekf/astro.config.ts
@@ -4,21 +4,18 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
+// return AstroUserConfig
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tomekf.pl/',
   compressHTML: true,
-  build: {
-    inlineStylesheets: 'always',
-  },
+  build: { inlineStylesheets: 'always' },
   trailingSlash: 'never',
   integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
+    tailwind({ applyBaseStyles: false }),
     mdx(),
     // sitemap(),
-    sitemap({ filter: (page) => !page.includes('/blog') && !page.includes('/test') }),
+    sitemap({ filter: (page) => !page.includes('/blog') }),
     react(),
   ],
   vite: {
