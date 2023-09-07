1264c5612cb06b13fa7682e2ee7385f55b748ff3
Tomek Fijoł <tomekfijol@gmail.com>
2023-09-07 22:33:09 +0200

    feat: [kysely] Add databases creation and example selects (#238)

--- pocs/kysely/src/index.ts
+++ pocs/kysely/src/index.ts
@@ -1,7 +1,38 @@
+import { sql } from 'kysely';
+
 import { db } from './database';
 
 (async () => {
-    const result = await db
+    // create tables
+    await db.schema
+        .createTable('person')
+        .addColumn('id', 'integer', (col) => col.primaryKey())
+        .addColumn('first_name', 'text', (col) => col.notNull())
+        .addColumn('last_name', 'text')
+        .addColumn('gender', 'text', (col) => col.notNull())
+        .addColumn('created_at', 'text', (col) =>
+            col.defaultTo(sql`CURRENT_TIMESTAMP`).notNull(),
+        )
+        .execute();
+
+    await db.schema
+        .createTable('pet')
+        .addColumn('id', 'integer', (col) => col.primaryKey())
+        .addColumn('name', 'text', (col) => col.notNull().unique())
+        .addColumn('owner_id', 'integer', (col) =>
+            col.references('person.id').onDelete('cascade').notNull(),
+        )
+        .addColumn('species', 'text', (col) => col.notNull())
+        .execute();
+
+    // await db.schema
+    //     .createIndex('pet_owner_id_index')
+    //     .on('pet')
+    //     .column('owner_id')
+    //     .execute();
+
+    // add Jennifer Aniston
+    await db
         .insertInto('person')
         .values({
             first_name: 'Jennifer',
@@ -10,5 +41,24 @@ import { db } from './database';
         })
         .executeTakeFirst();
 
-    console.log(result.insertId, result);
+    // get all persons
+    const persons = await db
+        .selectFrom('person')
+        .select(['person.id', 'first_name'])
+        .execute();
+
+    // get Jennifer Aniston
+    const person = await db
+        .selectFrom('person')
+        .select(['id', 'first_name', 'last_name', 'created_at', 'gender'])
+        .where('id', '=', 1)
+        .execute();
+
+    const person2 = await db
+        .selectFrom('person')
+        .selectAll()
+        .where('first_name', '=', 'Jennifer')
+        .execute();
+
+    console.log({ persons, person, person2 });
 })();
