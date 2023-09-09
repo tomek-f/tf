a6c205b046aa747416bc6b0165074a8c5f2eb51f
Tomasz Fijoł <tomasz.fijol@bold.com>
2023-09-09 15:19:19 +0200

    feat: [bun-hono] Add static files

--- pocs/bun-hono/src/index.ts
+++ pocs/bun-hono/src/index.ts
@@ -1,7 +1,16 @@
 import { Hono } from 'hono';
+import { serveStatic } from 'hono/bun';
 
 const app = new Hono();
 
-app.get('/', (context) => context.text('Hello Hono!'));
+app.use('/*', serveStatic({ root: './src' }));
+app.use('/favicon.ico', serveStatic({ path: './src/favicon.ico' }));
+app.get('/', (context) =>
+    context.text('Hello Hono! You can access: /static/hello.txt'),
+);
+app.get('*', serveStatic({ path: './src/static/fallback.txt' }));
 
-export default app;
+export default {
+    port: 2137,
+    fetch: app.fetch,
+};
