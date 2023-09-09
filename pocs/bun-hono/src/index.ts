188a65c8a8f7a402233706af42afc27d1f2b3c84
Tomasz Fijoł <tomasz.fijol@bold.com>
2023-09-09 20:55:30 +0200

    feat: [bun-hono] Add middleware

--- pocs/bun-hono/src/index.ts
+++ pocs/bun-hono/src/index.ts
@@ -3,11 +3,18 @@ import { serveStatic } from 'hono/bun';
 
 const app = new Hono();
 
+app.use('*', async (c, next) => {
+    const start = Date.now();
+
+    await next();
+
+    const end = Date.now();
+
+    c.res.headers.set('X-Response-Time', `${end - start}`);
+});
 app.use('/*', serveStatic({ root: './src' }));
 app.use('/favicon.ico', serveStatic({ path: './src/favicon.ico' }));
-app.get('/', (context) =>
-    context.text('Hello Hono! You can access: /static(s)/hello.txt'),
-);
+app.get('/', (c) => c.text('Hello Hono! You can access: /static(s)/hello.txt'));
 app.get(
     '/*',
     serveStatic({
