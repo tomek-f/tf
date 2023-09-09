323e946b29106869707294bc0946f1817ab3f7c6
Tomasz Fijoł <tomasz.fijol@bold.com>
2023-09-09 15:51:08 +0200

    feat: [bun-hono] Add rewrite

--- pocs/bun-hono/src/index.ts
+++ pocs/bun-hono/src/index.ts
@@ -6,7 +6,14 @@ const app = new Hono();
 app.use('/*', serveStatic({ root: './src' }));
 app.use('/favicon.ico', serveStatic({ path: './src/favicon.ico' }));
 app.get('/', (context) =>
-    context.text('Hello Hono! You can access: /static/hello.txt'),
+    context.text('Hello Hono! You can access: /static(s)/hello.txt'),
+);
+app.get(
+    '/*',
+    serveStatic({
+        root: './src',
+        rewriteRequestPath: (path) => path.replace(/^\/statics/, '/static'),
+    }),
 );
 app.get('*', serveStatic({ path: './src/static/fallback.txt' }));
 
