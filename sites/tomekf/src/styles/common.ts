7dd3f4a83a64e6380bff9df748eacfb9bf96429f
Tomasz Fijoł <tomasz.fijol@bold.com>
2023-09-16 10:38:13 +0200

    feat: [tomekf] Media changes

--- sites/tomekf/src/styles/common.ts
+++ sites/tomekf/src/styles/common.ts
@@ -8,4 +8,5 @@ export const linkHoverCss = 'hover:text-sky-300 hover:underline';
 
 export const linkTransitionCss = 'transition-colors duration-300';
 
+// sync with .custom-html a
 export const linkCss = `text-base ${linkDefaultCss} ${linkTransitionCss} ${linkHoverCss}`;
