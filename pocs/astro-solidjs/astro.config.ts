1699edbdd5cad347aa8ec03a0ffc4c7e8d6bf26c
Tomek Fijoł <tomekfijol@gmail.com>
2023-09-21 18:52:07 +0200

    feat: Remove sitemap from POC (#310)

--- pocs/astro-solidjs/astro.config.ts
+++ pocs/astro-solidjs/astro.config.ts
@@ -1,5 +1,3 @@
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 import solidJs from '@astrojs/solid-js';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
@@ -11,20 +9,5 @@ export default defineConfig({
     compressHTML: true,
     build: { inlineStylesheets: 'always' },
     trailingSlash: 'never',
-    integrations: [
-        tailwind({ applyBaseStyles: false }),
-        mdx(),
-        // sitemap(),
-        sitemap({
-            filter: (page) =>
-                !page.includes('/blog') && !page.includes('/media'),
-        }),
-        solidJs(),
-    ],
-    // vite: {
-    //     ssr: {
-    //         // external: ['@11ty/eleventy-img'],
-    //         // noExternal: ['@radix-ui/*'],
-    //     },
-    // },
+    integrations: [tailwind({ applyBaseStyles: false }), solidJs()],
 });
