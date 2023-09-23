7e8e4f5aa7ab70b3a4ca43d5017a319ab55781e5
Tomek Fijoł <tomekfijol@gmail.com>
2023-09-23 14:10:30 +0200

    feat: Add @tf/astro-ssr POC + other stuff; Restart lock file (#312)

---
+++ pocs/astro-ssr/astro.config.ts
@@ -0,0 +1,16 @@
+import nodejs from '@astrojs/node';
+import solidJs from '@astrojs/solid-js';
+import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
+
+// return AstroUserConfig
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://tomekf.pl/',
+    output: 'server',
+    adapter: nodejs({ mode: 'standalone' }),
+    compressHTML: true,
+    build: { inlineStylesheets: 'always' },
+    trailingSlash: 'never',
+    integrations: [tailwind({ applyBaseStyles: false }), solidJs()],
+});
