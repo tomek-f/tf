332ae7459ff730c546ea2d2d3f854b4ffa80a415
Tomasz Fijoł <tomasz.fijol@bold.com>
2023-09-23 14:00:21 +0200

    ssr changes

--- pocs/astro-ssr/astro.config.ts
+++ pocs/astro-ssr/astro.config.ts
@@ -1,3 +1,4 @@
+import nodejs from '@astrojs/node';
 import solidJs from '@astrojs/solid-js';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
@@ -6,6 +7,8 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://tomekf.pl/',
+    output: 'server',
+    adapter: nodejs({ mode: 'standalone' }),
     compressHTML: true,
     build: { inlineStylesheets: 'always' },
     trailingSlash: 'never',
