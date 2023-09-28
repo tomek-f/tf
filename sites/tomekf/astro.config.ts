c73d752af3664e33df2324d2e466bf01412f5ec6
Tomasz Fijoł <tomasz.fijol@bold.com>
2023-09-28 22:43:56 +0200

    feat: [astro-ssr] Add first api endpoint

--- sites/tomekf/astro.config.ts
+++ sites/tomekf/astro.config.ts
@@ -4,7 +4,7 @@ import solidJs from '@astrojs/solid-js';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
-// return AstroUserConfig
+// returns AstroUserConfig
 // https://astro.build/config
 export default defineConfig({
     site: 'https://tomekf.pl/',
