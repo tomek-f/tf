a6a0572712b3e3ebfc8cec41b688844f0612a147
Tomek Fijoł <tomekfijol@gmail.com>
2023-09-29 20:51:07 +0200

    feat: [astro-ssr] Add more api endpoints (#343)

---
+++ pocs/astro-ssr/src/pages/api/hello/index.ts
@@ -0,0 +1,12 @@
+import type { APIRoute } from 'astro';
+
+export const GET: APIRoute = async (data) => {
+    console.log([...data.url.searchParams]);
+
+    return new Response(JSON.stringify('Hello world!'), {
+        status: 200,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+};
