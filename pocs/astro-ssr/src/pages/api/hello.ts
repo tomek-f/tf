e99465278493ec73e1d512fb42c1d2f6d39d028e
Tomasz Fijoł <tomasz.fijol@bold.com>
2023-09-30 14:01:56 +0200

    feat: [astro-ssr] Change hello types and comments

--- pocs/astro-ssr/src/pages/api/hello.ts
+++ pocs/astro-ssr/src/pages/api/hello.ts
@@ -1,7 +1,11 @@
 import type { APIRoute } from 'astro';
 
+const data = { greeting: 'Hello' };
+
+export type HelloData = typeof data;
+
 export const GET: APIRoute = () => {
-    return new Response(JSON.stringify({ greeting: 'Hello' }), {
+    return new Response(JSON.stringify(data), {
         headers: { 'Content-Type': 'application/json' },
         status: 200,
     });
