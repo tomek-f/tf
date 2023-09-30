cda171e498df20e1c2fa2b31f929f2bdda08e456
Tomasz Fijoł <tomasz.fijol@bold.com>
2023-09-30 09:16:56 +0200

    feat: [astro-ssr] Add even more api endpoints

---
+++ pocs/astro-ssr/src/pages/api/test-post.json.ts
@@ -0,0 +1,15 @@
+import type { APIRoute } from 'astro';
+
+export const POST: APIRoute = async ({ request }) => {
+    if (request.headers.get('Content-Type') === 'application/json') {
+        const body = (await request.json()) as { name: string };
+        const name = body.name;
+
+        return new Response(
+            JSON.stringify({ message: 'Your name was: ' + name }),
+            { status: 200 },
+        );
+    }
+
+    return new Response(null, { status: 400 });
+};
