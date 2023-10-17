f754eb03c0db735005b95cbbfe1df4810451f024
Tomasz Fijoł <tomasz.fijol@bold.com>
2023-10-17 22:22:41 +0200

    init

---
+++ go/blog/go.mod
@@ -0,0 +1,8 @@
+module tomekf.pl/blog
+
+go 1.21.3
+
+require (
+	github.com/go-chi/chi/v5 v5.0.10
+	github.com/mattn/go-sqlite3 v1.14.17
+)
---
+++ go/blog/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	_ "database/sql"
+
+	_ "github.com/go-chi/chi/v5"
+	_ "github.com/go-chi/chi/v5/middleware"
+	_ "github.com/mattn/go-sqlite3"
+)
