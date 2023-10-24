27e4308c62bcf57301cb18a809b4c7a2523e069e
Tomasz Fijoł <tomasz.fijol@bold.com>
2023-10-24 09:08:08 +0200

    feat: [react-playground] Add vite changes

--- pocs/react-playground/vite.config.ts
+++ pocs/react-playground/vite.config.ts
@@ -2,35 +2,37 @@ import { resolve } from 'node:path';
 
 import react from '@vitejs/plugin-react-swc';
 import { visualizer } from 'rollup-plugin-visualizer';
-import { loadEnv, type UserConfig } from 'vite';
 
-/**
- * Replace env variables in index.html
- * @see https://github.com/vitejs/vite/issues/3105#issuecomment-939703781
- * @see https://vitejs.dev/guide/api-plugin.html#transformindexhtml
- */
-function htmlPlugin(environment: ReturnType<typeof loadEnv>) {
-    return {
-        name: 'html-transform',
-        transformIndexHtml: {
-            order: 'pre',
-            transform: (html: string) =>
-                // TODO ? fix this
-                // eslint-disable-next-line unicorn/prefer-string-replace-all
-                html.replace(
-                    /%(.*?)%/g,
-                    (match, p1: string) => environment[p1] ?? match,
-                ),
-        },
-    };
-}
+// import { loadEnv, type UserConfig } from 'vite';
+
+// not needed for VITE prefix https://vitejs.dev/guide/env-and-mode.html#html-env-replacement
+// /**
+//  * Replace env variables in index.html
+//  * @see https://github.com/vitejs/vite/issues/3105#issuecomment-939703781
+//  * @see https://vitejs.dev/guide/api-plugin.html#transformindexhtml
+//  */
+// function htmlPlugin(environment: ReturnType<typeof loadEnv>) {
+//     return {
+//         name: 'html-transform',
+//         transformIndexHtml: {
+//             order: 'pre',
+//             transform: (html: string) =>
+//                 // TODO ? fix this
+//                 // eslint-disable-next-line unicorn/prefer-string-replace-all
+//                 html.replace(
+//                     /%(.*?)%/g,
+//                     (match, p1: string) => environment[p1] ?? match,
+//                 ),
+//         },
+//     };
+// }
 
 // loadEnv(mode as string, process.cwd(), 'VITE') === loadEnv(mode as string, '.', 'VITE')
 // https://vitejs.dev/config/
-export default ({ mode }: UserConfig) => ({
+export default (/* { mode }: UserConfig */) => ({
     plugins: [
         react(),
-        htmlPlugin(loadEnv(mode as string, process.cwd(), 'VITE')),
+        // htmlPlugin(loadEnv(mode as string, process.cwd(), 'VITE')),
         // stats.html file sizes are messed up
         visualizer({
             // generates ./stats.html
