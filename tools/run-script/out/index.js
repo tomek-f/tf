1db6107bd746b399d197e44a289322b20aa509bd
Tomasz Fijoł <tomasz.fijol@bold.com>
2023-10-29 11:40:50 +0100

    up

--- tools/run-script/out/index.js
+++ tools/run-script/out/index.js
@@ -3,7 +3,6 @@ import { execSync } from 'node:child_process';
 import { readFileSync } from 'node:fs';
 import gradient from 'gradient-string';
 import inquirer from 'inquirer';
-
 var packageJson = JSON.parse(readFileSync('./package.json', 'utf8'));
 var scripts = packageJson.scripts;
 var keys = Object.keys(scripts);
@@ -15,7 +14,6 @@ var questions = [
         type: 'list',
     },
 ];
-
 console.log(gradient('#bada55', 'hotpink')('Hi, welcome to run-script!'));
 // TODO ? fix this
 // eslint-disable-next-line unicorn/prefer-top-level-await, @typescript-eslint/no-floating-promises
