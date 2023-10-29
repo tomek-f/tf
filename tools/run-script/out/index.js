07018ef02f6ec432b169443b4f51bb4e68951026
Tomek Fijoł <tomekfijol@gmail.com>
2023-10-29 11:50:32 +0100

    feat: Remove playwright; Change run-script (#400)
    Co-authored-by: Crazy Nugatt <crazynugatt@gmail.com>
    Co-authored-by: Tomek Fijoł <tomekfijol@gmail.com>

--- tools/run-script/out/index.js
+++ tools/run-script/out/index.js
@@ -1,26 +1,26 @@
 #!/usr/bin/env node
 import { execSync } from 'node:child_process';
 import { readFileSync } from 'node:fs';
-
 import gradient from 'gradient-string';
 import inquirer from 'inquirer';
-
-var packageJson = JSON.parse(readFileSync('./package.json', 'utf-8'));
+var packageJson = JSON.parse(readFileSync('./package.json', 'utf8'));
 var scripts = packageJson.scripts;
 var keys = Object.keys(scripts);
 var questions = [
-  {
-    type: 'list',
-    name: 'script',
-    message: 'Choose a script to run',
-    choices: keys,
-  },
+    {
+        choices: keys,
+        message: 'Choose a script to run',
+        name: 'script',
+        type: 'list',
+    },
 ];
-
 console.log(gradient('#bada55', 'hotpink')('Hi, welcome to run-script!'));
+// TODO ? fix this
+// eslint-disable-next-line unicorn/prefer-top-level-await, @typescript-eslint/no-floating-promises
 inquirer.prompt(questions).then(function (answers) {
-  execSync('npm run '.concat(answers.script), {
-    // cwd: process.cwd(),
-    stdio: [process.stdin, process.stdout, process.stderr],
-  });
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    execSync("npm run ".concat(answers.script), {
+        // cwd: process.cwd(),
+        stdio: [process.stdin, process.stdout, process.stderr],
+    });
 });
