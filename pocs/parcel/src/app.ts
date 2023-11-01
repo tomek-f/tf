d1770c7a7c8a38a0aa9523946514cba18a4bbe50
Tomek Fijoł <tomekfijol@gmail.com>
2023-11-01 11:42:41 +0100

    feat: Remove hono, falso and parcel stuff; Update readme (#404)

--- pocs/parcel/src/app.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable unicorn/prefer-module */
-console.log('Hello world!!!');
-
-// @ts-expect-error parcel-hmr
-if (module.hot) {
-    // @ts-expect-error parcel-hmr
-    module.hot.accept(); // eslint-disable-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-}
