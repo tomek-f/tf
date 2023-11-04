e61eb978abb5c2eca3e87e52a5e10098bf457f70
Tomek Fijoł <tomekfijol@gmail.com>
2023-11-05 00:44:49 +0100

    feat: [tomekf] Add env types; Add debug changes (#418)

--- sites/tomekf/src/env.d.ts
+++ sites/tomekf/src/env.d.ts
@@ -2,3 +2,14 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+    readonly IS_DEVELOPMENT?: boolean;
+    readonly IS_NON_PRODUCTION?: boolean;
+    readonly IS_PREVIEW?: boolean;
+    readonly IS_PRODUCTION?: boolean;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
