56af8b49c18cfe9f43a9b1b7329a0470fb9fdb2d
Tomasz Fijoł <tomasz.fijol@bold.com>
2023-11-04 23:04:39 +0100

    feat: [astro-ssr] Add vercel adapter

--- pocs/astro-ssr/astro.config.ts
+++ pocs/astro-ssr/astro.config.ts
@@ -1,7 +1,13 @@
 /* eslint-disable sort-keys */
-import nodejs from '@astrojs/node';
+// import nodejs from '@astrojs/node';
 import solidJs from '@astrojs/solid-js';
 import tailwind from '@astrojs/tailwind';
+// Use Vercel Edge Functions (Recommended)
+// import vercel from '@astrojs/vercel/edge';
+// Can also use Serverless Functions
+import vercel from '@astrojs/vercel/serverless';
+// Or a completely static build
+// import vercel from '@astrojs/vercel/static';
 import { defineConfig } from 'astro/config';
 
 // returns AstroUserConfig
@@ -9,7 +15,8 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
     site: 'https://tomekf.pl/',
     output: 'server',
-    adapter: nodejs({ mode: 'standalone' }),
+    // adapter: nodejs({ mode: 'standalone' }),
+    adapter: vercel(/* { imageService: true } */),
     compressHTML: true,
     build: { inlineStylesheets: 'always' },
     trailingSlash: 'never',
