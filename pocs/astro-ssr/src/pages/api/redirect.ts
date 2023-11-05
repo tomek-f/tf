70dd0cdf60aa0f4ab1793698d3fb3d6b59104e6b
Tomek Fijoł <tomekfijol@gmail.com>
2023-11-05 14:26:07 +0100

    feat: Merge asto ssr + astro solidjs; Cleanup package-lock.json (#420)

--- pocs/astro-ssr/src/pages/api/redirect.ts
+++ pocs/astro-ssr/src/pages/api/redirect.ts
@@ -1,21 +1,21 @@
 import type { APIRoute } from 'astro';
 
 export const GET: APIRoute = ({ url: { searchParams }, redirect }) => {
-    const a = searchParams.get('a') ?? '0';
+    const a = searchParams.get('a');
 
-    if (a === '111') {
-        return new Response(null, {
-            status: 404,
-            statusText: 'Not found',
-        });
-    }
-
-    if (a === '0') {
+    if (a === '1') {
         return new Response(JSON.stringify('Hello world!'), {
             headers: { 'Content-Type': 'application/json' },
             status: 200,
         });
     }
 
-    return redirect('fake-307', 307);
+    if (a === '2') {
+        return new Response(null, {
+            status: 404,
+            statusText: 'Not found',
+        });
+    }
+
+    return redirect('/fake-307', 307); // 307 -> 404 for /fake-307
 };
