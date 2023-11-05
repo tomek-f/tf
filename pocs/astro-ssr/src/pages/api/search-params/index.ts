caa00c96fa9ac8ea714c6a035d7e46bcfb54a5a7
Tomasz Fijoł <tomasz.fijol@bold.com>
2023-11-05 15:07:53 +0100

    feat: [astro-ssr] Add DB changes; Change other stuff

--- pocs/astro-ssr/src/pages/api/search-params/index.ts
+++ pocs/astro-ssr/src/pages/api/search-params/index.ts
@@ -1,9 +1,7 @@
 import type { APIRoute } from 'astro';
 
-export const GET: APIRoute = async (data) => {
-    console.log([...data.url.searchParams]);
-
-    return new Response(JSON.stringify([...data.url.searchParams]), {
+export const GET: APIRoute = async ({ url }) => {
+    return new Response(JSON.stringify([...url.searchParams]), {
         headers: { 'Content-Type': 'application/json' },
         status: 200,
     });
