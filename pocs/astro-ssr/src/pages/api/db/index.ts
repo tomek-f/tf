d07a386925257b07dea36ce80051c10f7c86a3c6
Tomek Fijoł <tomekfijol@gmail.com>
2023-11-05 15:15:44 +0100

    feat: [astro-ssr] Add DB changes; Change other stuff (#421)

--- pocs/astro-ssr/src/pages/api/db/index.ts
+++ pocs/astro-ssr/src/pages/api/db/index.ts
@@ -1,22 +1,29 @@
 import type { APIRoute } from 'astro';
 import SQLite from 'better-sqlite3';
 
-const __dirname = new URL('.', import.meta.url).pathname;
-const database = new SQLite(`${__dirname}/data.sqlite`, {
-    // verbose: console.log,
-});
+// const __dirname = new URL('.', import.meta.url).pathname;
+// const database = new SQLite(`${__dirname}/data.sqlite`, {
+//     // verbose: console.log,
+// });
+const database = new SQLite(':memory:');
 
+// CREATE TABLE IF NOT EXISTS articles
 database.exec(`
-    create table if not exists articles (
-        id integer not null primary key autoincrement,
-        title text,
-        content text
+    CREATE TABLE articles (
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        title TEXT,
+        content TEXT
     );
+    INSERT INTO articles(title, content)
+    VALUES
+        ('Article 1', 'Article 1 content'),
+        ('Article 2', 'Article 2 content'),
+        ('Article 3', 'Article 3 content');
 `);
 
-export const GET: APIRoute = async (data) => {
-    const stmt = database.prepare('select id, title, content from articles');
-    const pretty = data.url.searchParams.get('pretty');
+export const GET: APIRoute = async ({ url }) => {
+    const stmt = database.prepare('SELECT id, title, content FROM articles');
+    const pretty = url.searchParams.get('pretty');
     const articles = stmt.all();
 
     return new Response(JSON.stringify(articles, null, pretty ? 2 : 0), {
