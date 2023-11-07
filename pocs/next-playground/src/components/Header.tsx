559507c97428d80a8210ab9bc1fa5b2225955add
Tomasz Fijoł <tomasz.fijol@bold.com>
2023-11-07 19:57:15 +0100

    feat: [next-playground] Add logo gradients

--- pocs/next-playground/src/components/Header.tsx
+++ pocs/next-playground/src/components/Header.tsx
@@ -9,10 +9,10 @@ const Header = () => {
         <div className="navbar bg-base-300 flex-col rounded-md md:flex-row">
             <div className="flex-1">
                 <Link
-                    className={`font-agbalumo ${fontAgbalumo.variable} btn btn-ghost text-3xl normal-case`}
+                    className={`font-agbalumo ${fontAgbalumo.variable} bg-gradient-to-b from-emerald-800 to-emerald-500 bg-clip-text text-4xl text-transparent dark:from-pink-500 dark:to-pink-200`}
                     href="/"
                 >
-                    next-playground
+                    next playground
                 </Link>
             </div>
             <div className="flex-none">
