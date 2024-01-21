2cdc0ab3ab22dbebdf8052aab988a810e221e471
Tomek Fijoł <tomekfijol@gmail.com>
2024-01-21 13:21:09 +0100

    feat: Add HTMX example (#487)

--- pocs/astro-ssr/src/env.d.ts
+++ pocs/astro-ssr/src/env.d.ts
@@ -1,2 +1,8 @@
 /* eslint-disable  unicorn/prevent-abbreviations */
 /// <reference types="astro/client" />
+
+interface Window {
+    htmx: {
+        process: (element: HTMLElement) => void;
+    };
+}
