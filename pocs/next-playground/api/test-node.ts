c29ff29c48b65355e3a4d59695be7160ebf39a7e
Tomasz Fijoł <tomasz.fijol@bold.com>
2024-02-01 15:20:37 +0100

    up

--- pocs/next-playground/api/test-node.ts
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default function handler(request: any, response: any) {
-    const { name = 'World' } = request.query;
-
-    return response.send(`Hello ${name}!`);
-}
