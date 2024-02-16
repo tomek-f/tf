04d3896d01fae8a511c47e575f427c1ec02355e8
Tomasz Fijoł <tomasz.fijol@bold.com>
2024-02-16 17:38:55 +0100

    feat: [go] Add some examples to kitchesink

--- go/kitchensink/main.go
+++ go/kitchensink/main.go
@@ -1,9 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
 
 const elo string = "Elo!"
 const a int = 1
+
+func newCard() string {
+	return "Five of Diamonds"
+}
+
+func switchTest() {
+	fmt.Print("Go runs on ")
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux.")
+	default:
+		// freebsd, openbsd,
+		// plan9, windows...
+		fmt.Printf("%s.\n", os)
+	}
+
+	fmt.Print("Go runs on ")
+	os := runtime.GOOS
+	switch os {
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux.")
+	default:
+		// freebsd, openbsd,
+		// plan9, windows...
+		fmt.Printf("%s.\n", os)
+	}
+
+	fmt.Println("When's Saturday?")
+	today := time.Now().Weekday()
+	// fmt.Println(today, today+1)
+	switch time.Saturday {
+	case today + 0:
+		fmt.Println("Today.")
+	case today + 1:
+		fmt.Println("Tomorrow.")
+	case today + 2:
+		fmt.Println("In two days.")
+	default:
+		fmt.Println("Too far away.")
+	}
+}
 
 func main() {
 	var card string = "Ace of Spades"
@@ -21,8 +70,6 @@ func main() {
 	for i, card := range cards {
 		fmt.Println(i, card)
 	}
-}
 
-func newCard() string {
-	return "Five of Diamonds"
+	switchTest()
 }
