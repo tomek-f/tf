1f149fdf7c50839fb92ab239370380585befb1d7
Tomasz Fijoł <tomasz.fijol@bold.com>
2024-06-04 09:46:24 +0200

    feat: Remove astro ssr example from vercel

--- pocs/astro-ssr/astro.config.ts
+++ pocs/astro-ssr/astro.config.ts
@@ -1,13 +1,7 @@
 /* eslint-disable sort-keys */
-// import nodejs from '@astrojs/node';
+import nodejs from '@astrojs/node';
 import solidJs from '@astrojs/solid-js';
 import tailwind from '@astrojs/tailwind';
-// Use Vercel Edge Functions (Recommended)
-// import vercel from '@astrojs/vercel/edge';
-// Can also use Serverless Functions
-import vercel from '@astrojs/vercel/serverless';
-// Or a completely static build
-// import vercel from '@astrojs/vercel/static';
 import { defineConfig } from 'astro/config';
 
 // returns AstroUserConfig
@@ -15,8 +9,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
     site: 'https://tomekf.pl/',
     output: 'server',
-    // adapter: nodejs({ mode: 'standalone' }),
-    adapter: vercel(/* { imageService: true } */),
+    adapter: nodejs({ mode: 'standalone' }),
     compressHTML: true,
     build: { inlineStylesheets: 'always' },
     trailingSlash: 'never',
